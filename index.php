dd49393e4e95d9cc68a2161b32e27e65a2f7dede
D1vy4n5hu 5hukl4 <divyanshushukl@gmail.com>
2018-11-27 10:48:29 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>LFI Injection by SECOPS</title>
+</head>
+
+<body bgcolor="#000000">
+<div style=" margin-top:70px;color:#FFF; font-size:23px; text-align:center">Welcome&nbsp;&nbsp;&nbsp;<font color="#FF0000">Secops</font><br>
+<font size="3" color="#FFFF00">
+
+<h1> Welcome to my home page
+<p>Hello World</p>
+<?php include 'lfi.php';?>
+
+
+
+
+</font> </div></br></br></br><center>
+
+</body>
+</html>
---
+++ lfi.php
@@ -0,0 +1,4 @@
+<?php
+echo "Secops";
+echo "\033[01;31m Welcome to TRAiNing \033[0m";
+?>
